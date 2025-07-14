789185c1c684367e9531c7d86c89d9bd86a25b51
Marwanhoo <Muh.Marwanhoo@gmail.com>
2025-07-14 20:43:00 +0300

    feat: Customize video call controls
    This commit customizes the video call controls displayed during a call.
    
    The following controls are now available:
    - Chat
    - Flip Camera
    - Add Reaction
    - Toggle Microphone
    - Toggle Camera
    - Leave Call
    
    The `callContentBuilder` and `callControlsBuilder` properties of `StreamCallContainer` and `StreamCallContent` respectively are used to achieve this customization.

--- lib/main.dart
+++ lib/main.dart
@@ -89,6 +89,55 @@ class _CallScreenState extends State<CallScreen> {
     return Scaffold(
       body: StreamCallContainer(
         call: widget.call,
+        callContentBuilder: (
+            BuildContext context,
+            Call call,
+            CallState callState,
+            ) {
+          return StreamCallContent(
+            call: call,
+            callState: callState,
+            callControlsBuilder: (
+                BuildContext context,
+                Call call,
+                CallState callState,
+                ) {
+              final localParticipant = callState.localParticipant!;
+              return StreamCallControls(
+                options: [
+                  CallControlOption(
+                    icon: const Icon(Icons.chat_outlined),
+                    onPressed: () {
+                      // Open your chat window
+                    },
+                  ),
+                  FlipCameraOption(
+                    call: call,
+                    localParticipant: localParticipant,
+                  ),
+                  AddReactionOption(
+                    call: call,
+                    localParticipant: localParticipant,
+                  ),
+                  ToggleMicrophoneOption(
+                    call: call,
+                    localParticipant: localParticipant,
+                  ),
+                  ToggleCameraOption(
+                    call: call,
+                    localParticipant: localParticipant,
+                  ),
+                  LeaveCallOption(
+                    call: call,
+                    onLeaveCallTap: () {
+                      call.leave();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        },
       ),
     );
   }
